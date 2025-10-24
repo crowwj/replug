5b9e65d27e186bf5120f8a4dff09e790d5c3c82d
juan pablo <juanpablosto921@gmail.com>
2025-10-23 21:27:36 -0700

    pinchegit

--- resources/views/contenido.blade.php
+++ resources/views/contenido.blade.php
@@ -1,25 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-     <link rel="stylesheet" href="{{ asset('contenido.css') }}" />
+    <link rel="stylesheet" href="{{ asset('contenido.css') }}" />
 </head>
+
 <body>
 
+    <!-- Esta es la mera barra de navegacion (ya no)-->
+    <div class="barra-superior">
+        <div class="logo">
+            <img src="img/iconotrabajo.png" class="icono-logo" alt="logo guamutech">
+        </div>
 
-    <!-- Esta es la mera barra de navegacion-->
- <div class="navegador">
+        <div class="menu">
+            <div class="inicio">Inicio</div>
+            <div class="categorias">Categorias</div>
+            <input type="text" class="busqueda" placeholder="Buscar productos...">
+        </div>
+
+        <div class="perfil">
+            <img src="img/usuario.png" class="icono-perfil" alt="perfil">
+        </div>
+    </div>
+
     <nav class="navegacion">
         <ul>
             <li>
-                <a href=""></a>
+                <a href="">wasaa</a>
             </li>
             <li>
                 <form action="">
                     <input type="text" name="" id="">
-                            <button>
+                    <button>
                         <i></i>
                     </button>
                 </form>
@@ -27,15 +43,11 @@
             <li>
                 <a href="">Mi perfil</a>
             </li>
-            <li>
-
-            </li>
         </ul>
     </nav>
- </div>
-
     <!--Cierre Sesion-->
     <button>Cerrar Sesion</button>
 
 </body>
+
 </html>
