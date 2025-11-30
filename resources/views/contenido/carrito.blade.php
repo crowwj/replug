72f46d6980082d5dd7e1c9a6c29d9554f0710b49
Payasolin <PayasolinDeck@gmail.com>
2025-11-29 22:19:46 -0700

    Carrito basico

--- resources/views/contenido/carrito.blade.php
+++ resources/views/contenido/carrito.blade.php
@@ -3,14 +3,32 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="{{ asset('contenido.css') }}" /> 
+        <link rel="stylesheet" href="{{ asset('DV325G') }}" /> 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-..." crossorigin="anonymous">
         <title>Document</title>
     </head>
 
     <body>
         <nav class="barra-ayuda">
-            <a href="" style="text-decoration: none; color: white;">Inicio</a>   
+            <a href="" style="text-decoration: none; color: white;">Inicio</a>
+            <div>
+                <a class="nav-link dropdown-toggle account-link-custom" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Mi Cuenta
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Datos de tu cuenta</a></li>
+                    <li><a class="dropdown-item" href="#">Seguridad e inicio de sesion</a></li>
+                    <li><a class="dropdown-item" href="#">Pedidos y compras</a></li>
+                    <li><a class="dropdown-item" href="#">Direcciones</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li>
+                        <a class="dropdown-item" href="{{ route('cerrarsesion') }}"onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Cerrar sesión</a>
+                        <form id="logout-form" action="{{ route('cerrarsesion') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
+            </div>
         </nav>
 
 
@@ -19,12 +37,20 @@
 
             <div class="carrito">
                 <div class="card card-grande">
-                    <span>Productos</span>
+                    <span class="t2c">Productos</span>
                 </div>
             </div>
             <div class="cuadropagar"> 
                 <div class="card card-cobro">
-                    <span>Resumen de la compra</span>
+                    <div class=t1c>
+                        <span>Resumen de la compra</span><hr>
+                    </div>
+                    <div class=nc>
+                        <span>Productos</span> <hr>
+                        <span style="color: green;">Envío</span><hr>
+                        <span>Total</span><hr>
+                    </div>
+                    <button type="button" class="btn btn-primary">Comprar</button>
                 </div>
             </div>
 
