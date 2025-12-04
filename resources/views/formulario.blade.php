8cc225609131bf97e4d2c7f0ad75c766edae3ecf
Payasolin <PayasolinDeck@gmail.com>
2025-12-03 17:46:05 -0700

    Signo interrogacion

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -21,7 +21,7 @@
   <button type="submit" class="btn btn-primary " style="background:#3c0050">Enviar</button> <br>
   </div>
   <div class="cuenta_nueva">
-  <p>No tienes cuenta? <a href="{{ route('registroform') }}">registrate</a></p>
+  <p>¿No tienes cuenta? <a href="{{ route('registroform') }}">Registrate</a></p>
    @error('correo')
   <div class="invalid-feedback" style="display: block;">
             {{ $message }}
